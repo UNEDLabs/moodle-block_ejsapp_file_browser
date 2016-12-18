b08e50f16ec3cd8fb8686a54328ab99fe89498e3
Luis de la Torre <ldelatorre@dia.uned.es>
2016-12-18 23:37:17 +0100

    Changes in how the buttons are implemented. They now all use either html_writer or $OUTPUT methods.

--- block_ejsapp_file_browser.php
+++ block_ejsapp_file_browser.php
@@ -36,7 +36,7 @@
 /**
  * Class that defines the EJSApp File Browser block.
  */ 
-class block_ejsapp_file_browser extends block_base {
+class block_ejsapp_file_browser extends block_list {
 
     /**
      * init function for the EJSApp File Browser block
@@ -62,6 +62,17 @@ class block_ejsapp_file_browser extends block_base {
     }
 
     /**
+     * Add custom html attributes to aid with theming and styling
+     *
+     * @return array
+     */
+    function html_attributes() {
+        $attributes = parent::html_attributes();
+        $attributes['class'] .= ' block_'. $this->name(); // Append our class to class attribute
+        return $attributes;
+    }
+
+    /**
      * instance_allow_multiple function for the EJSApp File Browser block
      */
     function instance_allow_multiple()
@@ -73,52 +84,53 @@ class block_ejsapp_file_browser extends block_base {
     * Defines the content of the block.
     *
     * @no params
-    * @return array The content of the block
+    * @return stdClass The content of the block
     */
     function get_content()
     {
         global $CFG, $PAGE, $OUTPUT;
 
-        if ($this->content !== null) {
+        if ($this->content !== NULL) {
             return $this->content;
         }
+
         if (empty($this->instance)) {
             return null;
         }
 
         $this->content = new stdClass();
-        $this->content->text = '';
+        $this->content->items = array();
+        $this->content->icons = array();
+        $this->content->footer = '';
         
         if (isloggedin() && !isguestuser()) { // Show the block
             $this->content = new stdClass();
-            $refresh_button = html_writer::start_tag('input', array('type'=>'image', 'id'=>'refreshEJSAppFBBut', 'src'=>$CFG->wwwroot.'/blocks/ejsapp_file_browser/pix/refresh.png', 'width'=>'25', 'height'=>'25'));
+            $this->content->items[0] = html_writer::tag('input', '', array('type'=>'image', 'id'=>'refreshEJSAppFBBut', 'src'=>$CFG->wwwroot.'/blocks/ejsapp_file_browser/pix/refresh.png', 'width'=>'25', 'height'=>'25'));
             $renderer = $this->page->get_renderer('block_ejsapp_file_browser');
-            $this->content->text = $refresh_button . $renderer->ejsapp_file_browser_tree();
+            $this->content->items[1] = $renderer->ejsapp_file_browser_tree();
             if (has_capability('moodle/user:manageownfiles', $this->context)) {
-                $manage_files_button = $OUTPUT->single_button(new moodle_url('/user/files.php', array('returnurl'=>$PAGE->url->out())), get_string('managemyfiles', 'block_ejsapp_file_browser'), 'get');
-                $this->content->text .= $manage_files_button;
+                $this->content->items[2] = $OUTPUT->single_button(new moodle_url('/user/files.php', array('returnurl'=>$PAGE->url->out())), get_string('managemyfiles', 'block_ejsapp_file_browser'), 'get');
                 if (strpos($PAGE->url, 'mod/ejsapp/view.php') !== false) { //inside an ejsapp activity
                     $param_init_buttons_states = array(false);
                     if (isset($_GET['rec_file'])) {
                         $param_init_buttons_states = array(true);
                     }
                     $this->page->requires->js_call_amd('block_ejsapp_file_browser/buttons_states', 'init', $param_init_buttons_states);
-                    $this->content->text .= html_writer::start_tag('fieldset') . html_writer::tag('legend', get_string('capture_legend', 'block_ejsapp_file_browser'), array('class' => 'recording')) .
+                    $this->content->footer = html_writer::start_tag('fieldset') . html_writer::tag('legend', get_string('capture_legend', 'block_ejsapp_file_browser'), array('class' => 'recording')) .
                         html_writer::div(html_writer::tag('button', get_string('show_capture_options', 'block_ejsapp_file_browser'), array('id' => 'show')) . html_writer::tag('button', get_string('hide_capture_options', 'block_ejsapp_file_browser'), array('id' => 'hide')), 'optionsBut') .
                         html_writer::end_tag('fieldset');
-                    $content1 = html_writer::start_tag('input', array('type' => 'submit', 'name' => 'startCapture', 'id' => 'startCaptureBut', 'value' => get_string('start_capture', 'block_ejsapp_file_browser'))) .
-                        html_writer::start_tag('input', array('type' => 'submit', 'name' => 'stopCapture', 'id' => 'stopCaptureBut', 'value' => get_string('stop_capture', 'block_ejsapp_file_browser'))) .
-                        html_writer::start_tag('input', array('type' => 'submit', 'name' => 'resetCapture', 'id' => 'resetCaptureBut', 'value' => get_string('reset_capture', 'block_ejsapp_file_browser')));
+                    $content1 = html_writer::empty_tag('input', array('type' => 'submit', 'name' => 'startCapture', 'id' => 'startCaptureBut', 'value' => get_string('start_capture', 'block_ejsapp_file_browser'))) .
+                        html_writer::empty_tag('input', array('type' => 'submit', 'name' => 'stopCapture', 'id' => 'stopCaptureBut', 'value' => get_string('stop_capture', 'block_ejsapp_file_browser'))) .
+                        html_writer::empty_tag('input', array('type' => 'submit', 'name' => 'resetCapture', 'id' => 'resetCaptureBut', 'value' => get_string('reset_capture', 'block_ejsapp_file_browser')));
                     $content = html_writer::div($content1, 'recordCapture');
-                    $content2 = html_writer::start_tag('input', array('type' => 'submit', 'name' => 'playCapture', 'id' => 'playCaptureBut', 'value' => get_string('play_capture', 'block_ejsapp_file_browser'))) .
+                    $content2 = html_writer::empty_tag('input', array('type' => 'submit', 'name' => 'playCapture', 'id' => 'playCaptureBut', 'value' => get_string('play_capture', 'block_ejsapp_file_browser'))) .
                         html_writer::label(get_string('change_speed', 'block_ejsapp_file_browser'), 'stepCapture', true, array('class' => 'velocity')) .
-                        html_writer::start_tag('input', array('type' => 'range', 'class' => 'stepCapture', 'name' => 'stepCapture', 'id' => 'stepCaptureBut', 'value' => '0', 'step' => '0.5', 'min' => '-4', 'max' => '4'));
+                        html_writer::empty_tag('input', array('type' => 'range', 'class' => 'stepCapture', 'name' => 'stepCapture', 'id' => 'stepCaptureBut', 'value' => '0', 'step' => '0.5', 'min' => '-4', 'max' => '4'));
                     $content .= html_writer::div($content2, 'playCapture');
                     $content = html_writer::div($content, 'captureInteraction', array('id' => 'captureInteraction', 'style' => 'display:none'));
-                    $this->content->text .= $content;
+                    $this->content->footer .= $content;
                 }
             }
-            $this->content->footer = '';
         }
 
         return $this->content;
@@ -128,5 +140,5 @@ class block_ejsapp_file_browser extends block_base {
      * enabling global configuration
      */
     function has_config() {return true;}
-}
 
+}
--- version.php
+++ version.php
@@ -33,10 +33,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2016100100;
+$plugin->version = 2016121800;
 $plugin->requires = 2013111800;
 $plugin->cron = 0;
 $plugin->component = 'block_ejsapp_file_browser'; // To check on upgrade, that module sits in correct place
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.3 (Build: 2016100100)';
+$plugin->release = '1.3 (Build: 2016121800)';
 $plugin->dependencies = array('mod_ejsapp' => 2014121300);
